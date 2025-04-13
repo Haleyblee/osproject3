753d5e1d75011629bc792a2919ff063d91c78fe2
Haleyblee <49417946+Haleyblee@users.noreply.github.com>
2025-04-13 03:15:49 -0500

    Update strf_process_sched.h

--- strf_process_sched.h
+++ strf_process_sched.h
@@ -1,3 +1,6 @@
+#ifndef SRTF_PROCESS_SCHED_H
+#define SRTF_PROCESS_SCHED_H
+
 typedef struct {
     char id;
     float arrival_time;
@@ -6,11 +9,14 @@ typedef struct {
     int start_time;
     int end_time;
     int completed;
-    int response_time_set;
+    int first_start_time;
     int response_time;
+    int has_started;
 } Process;
 
 void read_input_file(const char* filename, int* context_switch, Process** process_list, int* num_processes);
 void simulate_srtf(Process* processes, int num_processes, int context_switch_time);
 void calculate_and_display_metrics(Process* processes, int num_processes);
 void free_memory(Process* processes);
+
+#endif
